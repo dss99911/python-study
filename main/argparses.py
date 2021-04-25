88c34516f0fee6a938ff4d89d2477ba1b9907237
Hyun <hyun@balancehero.com>
2021-04-26 03:49:00 +0900

    add

---
+++ main/argparses.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--decimal", dest="decimal", action="store")  # extra value
+parser.add_argument("-f", "--fast", dest="fast", action="store_true")  # existence/nonexistence
+args = parser.parse_args()
+
+print(args.decimal)
+print(args.fast)
+# 1
+# True
