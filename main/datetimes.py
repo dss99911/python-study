e958becd2724e9def1ccee2bf99515635d683b1c
Hyun <hyun@balancehero.com>
2021-04-29 04:58:18 +0900

    add

--- main/datetimes.py
+++ main/datetimes.py
@@ -1,9 +1,14 @@
 # dates are easily constructed and formatted
-from datetime import date
-now = date.today() # datetime.date(2003, 12, 2)
+from datetime import date, datetime, timedelta
+today = date.today() # datetime.date(2003, 12, 2)
+now = datetime.now()
+nine_hour = now + timedelta(hours=9)
 now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B.")  # '12-02-03. 02 Dec 2003 is a Tuesday on the 02 day of December.'
 
 # dates support calendar arithmetic
 birthday = date(1964, 7, 31)
-age = now - birthday
+age = today - birthday
 age.days  # 14368
+
+if __name__ == '__main__':
+    print(nine_hour < now)
--- main/subprocesses.py
+++ main/subprocesses.py
@@ -8,12 +8,23 @@ import subprocess
 
 # able to run shell script
 
-# run on same thread
-subprocess.run("ls", stdout=subprocess.PIPE, shell=True)
+def run_on_same_thread():
+    subprocess.run("ls", stdout=subprocess.PIPE, shell=True)
 
 # run on difference process
-process = subprocess.Popen("ls",shell=True, preexec_fn=os.setsid)
-time.sleep(20)
+def run_on_different_process():
+    process = subprocess.Popen("ls && sleep 5",shell=True, preexec_fn=os.setsid)
+    time.sleep(1)
+    poll = process.poll()
+    if poll is None:
+        print("running")
+    else:
+        print(f"not running {poll}")
+    # kill process
+    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
 
-# kill process
-os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+
+
+
+if __name__ == '__main__':
+    run_on_different_process()
