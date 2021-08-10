b44b48e5e40137e8b42a8cb8479b5a73381ed27c
Hyun <hyun@balancehero.com>
2021-08-10 13:12:25 +0900

    add

--- main/basic/reduces.py
+++ main/basic/reduces.py
@@ -5,3 +5,11 @@ lis = [1, 3, 5, 6, 2, ]
 # using reduce to compute sum of list
 print("The sum of the list elements is : ", end="")
 print(reduce(lambda a, b: a+b, lis))
+#%%
+# reduce with initial value
+
+
+def add(d, i):
+    d.append(i)
+    return d
+print(reduce(lambda d, i : add(d, i), lis, []))
