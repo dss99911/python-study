729641e8f6e254ed68551da478e0059d61f78ae2
Hyun <hyun@balancehero.com>
2022-04-07 14:56:48 +0900

    update

--- main/scrapping.py
+++ main/scrapping.py
@@ -19,3 +19,13 @@ tags = soup.select("#tab_con1 > div:nth-child(3) > table > tbody > tr.strong > t
 tags = soup.select("#tab_con1 > div:nth-of-type(2) > table > tbody > tr.strong > td > em")
 tag = tags[0]
 print(tag.text)
+
+#%%
+url = "https://play.google.com/store/search?q=personal%20loan&c=apps"
+html = requests.get(url).text
+soup = BeautifulSoup(html, "html5lib")
+tags = soup.select("div.b8cIId.ReQCgd.Q9MA7b > a")
+
+# use tag.contents for access inner tags
+result = [(tag["href"].split("=")[1], tag.contents[0].text) for tag in tags]
+#%%
